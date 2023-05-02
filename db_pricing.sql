c9a3dcc83de5728a287115752494852370469f34
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-02 14:13:17 +0200

    updated db

--- db_pricing.sql
+++ db_pricing.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.0.30 - MySQL Community Server - GPL
--- Server OS:                    Win64
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           8.0.30 - MySQL Community Server - GPL
+-- SE du serveur:                Win64
 -- HeidiSQL Version:             12.1.0.6537
 -- --------------------------------------------------------
 
@@ -15,24 +15,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Dumping database structure for landing_page
+-- Listage de la structure de la base pour landing_page
 CREATE DATABASE IF NOT EXISTS `landing_page` /*!40100 DEFAULT CHARACTER SET utf32 COLLATE utf32_swedish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `landing_page`;
 
--- Dumping structure for table landing_page.pricing
+-- Listage de la structure de table landing_page. pricing
 CREATE TABLE IF NOT EXISTS `pricing` (
   `id_pricing` int NOT NULL AUTO_INCREMENT,
   `price` float(8,2) NOT NULL DEFAULT '0.00',
-  `icon` int DEFAULT NULL,
-  `sale` int DEFAULT NULL,
+  `icon` int NOT NULL,
+  `sale` int NOT NULL,
+  `bandwidth` varchar(50) CHARACTER SET utf32 COLLATE utf32_swedish_ci NOT NULL,
+  `onlineSpace` varchar(50) COLLATE utf32_swedish_ci NOT NULL,
+  `supportNo` varchar(50) COLLATE utf32_swedish_ci NOT NULL,
+  `domain` varchar(50) COLLATE utf32_swedish_ci NOT NULL DEFAULT '',
+  `hiddenFees` varchar(50) COLLATE utf32_swedish_ci NOT NULL,
+  `name` varchar(50) COLLATE utf32_swedish_ci NOT NULL,
   PRIMARY KEY (`id_pricing`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf32 COLLATE=utf32_swedish_ci;
 
--- Dumping data for table landing_page.pricing: ~3 rows (approximately)
-INSERT INTO `pricing` (`id_pricing`, `price`, `icon`, `sale`) VALUES
-	(1, 9.99, 1, 0),
-	(2, 19.99, 1, 20),
-	(3, 29.99, 1, 0);
+-- Listage des données de la table landing_page.pricing : ~3 rows (environ)
+INSERT INTO `pricing` (`id_pricing`, `price`, `icon`, `sale`, `bandwidth`, `onlineSpace`, `supportNo`, `domain`, `hiddenFees`, `name`) VALUES
+	(1, 9.99, 1, 0, '1 GB', '500 Mb', 'No', '1', 'No', 'Starter'),
+	(2, 19.99, 1, 20, '2 GB', '1 GB', 'Yes', '3', 'No', 'Intermediate'),
+	(3, 29.99, 1, 0, '3 GB', '2 GB', 'Yes', 'Unlimited', 'No', 'Advanced');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
