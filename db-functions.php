5ca3ddba70bb15243ef78dbf7725eec3d385bf09
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-04 16:35:43 +0200

    rewritten  db functions t,fatal error handle

--- db-functions.php
+++ db-functions.php
@@ -1,4 +1,18 @@
 <?php
+session_start();
+
+function handleShutdown() {
+    $error = error_get_last();
+    if ($error !== null && $error['type'] === E_ERROR) {        //If there is an error and it is Fatal
+      // A fatal error occurred - store message in session
+      $_SESSION["admin"]== "There is an error somewhere";
+      header('Location: admin.php');
+      exit();
+    }
+  }
+  
+register_shutdown_function('handleShutdown');
+
 function connexion()
 {
     try {
@@ -9,7 +23,8 @@ function connexion()
             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
         );
         return $mySQLconnection;
-    } catch (Exception $e) {
+    } catch (\Exception $e) 
+    { 
         die('Erreur : ' . $e->getMessage());
     }
 }
@@ -17,41 +32,120 @@ function connexion()
 function getPricings()
 {
     $mySQLconnection = connexion();
-    $sqlQuery =    'SELECT * , FORMAT(price, 2, "fr_FR") AS "priceF" FROM pricing LIMIT 3'; //priceF means priceFormated
+    $sqlQuery = 'SELECT * , FORMAT(price, 2, "fr_FR") AS "priceF" FROM pricing LIMIT 3'; //priceF means priceFormated
     $persoLieuStatement = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
     $persoLieuStatement->execute();                                                     //The data we retrieve are in array form
     $pricings = $persoLieuStatement->fetchAll();
     return $pricings;
 }
 
+// fonction d'origine
+// function updatePricings()
+// {
+//     foreach ($_POST as $fieldName=>$value)              //looping over all field values
+//     {
+//         if (!empty($value) && $fieldName !== "id_pricing") //id is a field in hidden form
+//         {
+//             switch ($fieldName)
+//             {
+//                 case "price":
+//                     filter_input(INPUT_POST, $fieldName, FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);   //filtering data
+//                     $sqlQuery = 'UPDATE pricing             /*SQL request*/
+//                     SET '. $fieldName .' = :'.$fieldName.'
+//                     WHERE id_pricing = :id_pricing';
+//                     $mySQLconnection = connexion();
+//                     $persoLieuStatement = $mySQLconnection->prepare($sqlQuery);
+//                     $value = str_replace(",",".",$value);
+//                     $persoLieuStatement->bindValue($fieldName, $value, PDO::PARAM_STR);
+//                     $persoLieuStatement->bindValue('id_pricing', $_POST["id_pricing"], PDO::PARAM_INT);
+//                     $persoLieuStatement->execute();   
+//                 break;
+//                 case "sale":
+//                     filter_input(INPUT_POST, $fieldName, FILTER_VALIDATE_INT);   //filtering data
+//                     $sqlQuery = 'UPDATE pricing             /*SQL request*/
+//                     SET '. $fieldName .' = :'.$fieldName.' 
+//                     WHERE id_pricing = :id_pricing';
+//                     $mySQLconnection = connexion();
+//                     $persoLieuStatement = $mySQLconnection->prepare($sqlQuery);
+//                     $persoLieuStatement->bindValue($fieldName, $value, PDO::PARAM_STR);
+//                     $persoLieuStatement->bindValue('id_pricing', $_POST["id_pricing"], PDO::PARAM_INT);
+//                     $persoLieuStatement->execute();   
+//                 break;
+
+//                 default:
+//                     filter_input(INPUT_POST, $fieldName, FILTER_SANITIZE_FULL_SPECIAL_CHARS);   //filtering data
+//                     $sqlQuery = 'UPDATE pricing             /*SQL request*/
+//                     SET '. $fieldName .' = :'.$fieldName.' 
+//                     WHERE id_pricing = :id_pricing';
+//                     $mySQLconnection = connexion();
+//                     $persoLieuStatement = $mySQLconnection->prepare($sqlQuery);
+//                     $persoLieuStatement->bindValue($fieldName, $value, PDO::PARAM_STR);
+//                     $persoLieuStatement->bindValue('id_pricing', $_POST["id_pricing"], PDO::PARAM_INT);
+//                     $persoLieuStatement->execute();
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+// fonction améliorée, en suivant la même logique
 function updatePricings()
 {
+    $sqlQuerySetPart = "SET ";
+    $fieldsNameValue = [];
 
-
-    foreach ($_POST as $fieldName=>$value)              //looping over all field values
+    foreach ($_POST as $fieldName => $value)              //looping over all field values
     {
-        if (!empty($value) && $fieldName !== "id_pricing") //id is a field in hidden form
-    {       
-            filter_input(INPUT_POST, $fieldName, FILTER_SANITIZE_FULL_SPECIAL_CHARS);   //filtering data
-            $sqlQuery = 'UPDATE pricing             /*SQL request*/
-            SET '. $fieldName .' = :'.$fieldName.' 
-            WHERE id_pricing = :id_pricing';
-            $mySQLconnection = connexion();
-            $persoLieuStatement = $mySQLconnection->prepare($sqlQuery);
-            $persoLieuStatement->bindValue($fieldName, $value, PDO::PARAM_STR);
-            $persoLieuStatement->bindValue('id_pricing', $_POST["id_pricing"], PDO::PARAM_STR);
-            $persoLieuStatement->execute();       
+        if (!empty($value)) //id is a field in hidden form
+        {
+            $filteredValue = null;
+
+            switch ($fieldName)
+            {
+                case "price":
+                    $value = str_replace("," , "." , $value);
+                    $filteredValue = filter_var($value, FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);   //filtering data. F commas :-)
+                    echo "VIRGULE"; 
+                    var_dump($filteredValue);
+                break;
+
+                case "id_pricing":
+                case "sale":
+                    $filteredValue = filter_input(INPUT_POST, $fieldName, FILTER_VALIDATE_INT);   //filtering data using input because we can use it raw
+                break;
+
+                default:
+                    $filteredValue = filter_input(INPUT_POST, $fieldName, FILTER_SANITIZE_FULL_SPECIAL_CHARS);   //filtering data
+                break;
+            }
+            
+            $fieldsNameValue[$fieldName] = $filteredValue;
+            // $sqlQuerySetPart .= $fieldName . " = " . $filteredValue . ", ";
+            $sqlQuerySetPart .= $fieldName . " = :" . $fieldName . ", ";
         }
     }
-}       
+
+    echo "CHECK >> <br />>";
+    var_dump($fieldsNameValue);
+
+    $sqlQuerySetPart = rtrim($sqlQuerySetPart, ", ");
+
+    $sqlQuery = 'UPDATE pricing '. $sqlQuerySetPart
+                . ' WHERE id_pricing = :id_pricing';
+    var_dump($sqlQuery);
+    $mySQLconnection = connexion();
+    $persoLieuStatement = $mySQLconnection->prepare($sqlQuery);
+    $persoLieuStatement->bindValue($fieldName, $value, PDO::PARAM_STR); // TODO DUPLIQUER
+    $persoLieuStatement->execute($fieldsNameValue);
+}
 
 function updateQuantity()
 {
     $sqlQuery = 'UPDATE pricing SET commandes = (commandes + 1)
-    WHERE id_pricing = :id_pricing';
+        WHERE id_pricing = :id_pricing';
     $mySQLconnection = connexion();
     $persoLieuStatement = $mySQLconnection->prepare($sqlQuery);
-    $persoLieuStatement->bindValue('id_pricing', $_POST["id_pricing"], PDO::PARAM_STR);
+    $persoLieuStatement->bindValue('id_pricing', $_POST["id_pricing"], PDO::PARAM_INT);
     $persoLieuStatement->execute();       
 }
 
